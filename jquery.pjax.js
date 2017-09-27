3646e437e241e71afa23e56129452ee209e275e9
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2017-09-27 13:03:34 +0300

    Fixed PJAX initialization not to modify history, when it is disabled

--- jquery.pjax.js
+++ jquery.pjax.js
@@ -391,7 +391,8 @@ function pjax(options) {
       timeout: options.timeout,
       cache: options.cache
     }
-    window.history.replaceState(pjax.state, document.title)
+    if (options.history)
+      window.history.replaceState(pjax.state, document.title)
   }
 
   // New request can not override the existing one when option skipOuterContainers is set to true
